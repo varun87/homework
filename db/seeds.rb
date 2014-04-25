f94dde54fe35dba410194c0b4741fbbb4af91177
vagrant <vagrant@kellogg-dev-box>
2014-04-25 15:34:36 +0000

    First commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-diceimage = dice.create("name" => '1', "photo_url" => http://kiei925.com/images/dice/1.png)
-diceimage = dice.create("name" => '2', "photo_url" => http://kiei925.com/images/dice/2.png)
-diceimage = dice.create("name" => '3', "photo_url" => http://kiei925.com/images/dice/3.png)
-diceimage = dice.create("name" => '4', "photo_url" => http://kiei925.com/images/dice/4.png)
-diceimage = dice.create("name" => '5', "photo_url" => http://kiei925.com/images/dice/5.png)
-diceimage = dice.create("name" => '6', "photo_url" => http://kiei925.com/images/dice/6.png)
 
 
 Book.create(:title => "A Brief History of Time", :author => "Stephen Hawking")
